43eb1af3a58eb388de5253468714c9d7dd22b135
Arthur Felipe Carminati <76189417+CarminatiUser@users.noreply.github.com>
2023-04-13 21:45:37 -0300

    Update ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_01.cpp

--- ProgramaçãoEmLinguagemDeMontagem/ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_01.cpp
+++ ProgramaçãoEmLinguagemDeMontagem/ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_01.cpp
@@ -1,68 +1,65 @@
 #include <iostream>
-
 using namespace std;
-
-
 const int TAM = 8;
 
 //Disciplina: Arquitetura e Organização de Computadores
 //Atividade: Avaliação 01 – Programação em Linguagem de Montagem
 //Programa 01
 
-//Grupo: 
+//Grupo:
 //- Arthur Felipe Carminati
 //- Henrique Valiati Schroeder
 //- Vinícius Borchardt Grisa
 
 int main() {
-    int Vetor_A[TAM] = {0}; // vetor com 8 elementos inicializados em 0
-    int Vetor_B[TAM] = {0}; // vetor com 8 elementos inicializados em 0
-    int vetor_size = 0; // tamanho dos vetores
-    int temp = 0; // variável temporária para troca de valores
-    
-    // solicita o tamanho dos vetores
-    cout << "Entre com o tamanho dos vetores (máx. = 8): ";
-    cin >> vetor_size;
-    
-    // verifica se o tamanho é válido
-    while (vetor_size < 2 || vetor_size > TAM) {
-        cout << "Valor inválido. Entre com o tamanho dos vetores (máx. = 8): ";
-        cin >> vetor_size;
+    int Vetor_A[TAM] = {0}; // vetor de tamanho 8 inicializados em 0
+    int Vetor_B[TAM] = {0}; // vetor de tamanho 8 inicializados em 0
+    int tam_vetor = 0; // tamanho dos vetores inicializado em 0
+    int x = 0; // variável temporária para troca de valores
+   
+    // solicita ao usuário o tamanho dos vetores
+    cout << "Informe o tamanho dos vetores (máx. = 8): ";
+    cin >> tam_vetor;
+   
+    // verifica se o tamanho é válido (abaixo de 8 e maior que 0)
+    while (tam_vetor < 2 || tam_vetor > TAM) {
+        cout << "Valor inválido. Informe um valor menor que 8 e maior que 0): ";
+        cin >> tam_vetor;
     }
-    
+   
     // lê todos os elementos do Vetor_A
     cout << "Entre com os elementos do Vetor_A:\n";
-    for (int i = 0; i < vetor_size; i++) {
+    for (int i = 0; i < tam_vetor; i++) {
         cout << "Vetor_A[" << i << "] = ";
         cin >> Vetor_A[i];
     }
-    
+   
     // lê todos os elementos do Vetor_B
     cout << "Entre com os elementos do Vetor_B:\n";
-    for (int i = 0; i < vetor_size; i++) {
+    for (int i = 0; i < tam_vetor; i++) {
         cout << "Vetor_B[" << i << "] = ";
         cin >> Vetor_B[i];
     }
-    
+   
     // troca os valores dos elementos dos vetores de mesmo índice
-    for (int i = 0; i < vetor_size; i++) {
-        temp = Vetor_A[i];
+    for (int i = 0; i < tam_vetor; i++) {
+        x = Vetor_A[i];
         Vetor_A[i] = Vetor_B[i];
-        Vetor_B[i] = temp;
+        Vetor_B[i] = x;
     }
-    
     // imprime novamente os vetores
     cout << "Novo Vetor_A:\n";
-    for (int i = 0; i < vetor_size; i++) {
+    for (int i = 0; i < tam_vetor; i++) {
         cout << "Vetor_A[" << i << "] = " << Vetor_A[i] << endl;
     }
-    
+   
     cout << "Novo Vetor_B:\n";
-    for (int i = 0; i < vetor_size; i++) {
+    for (int i = 0; i < tam_vetor; i++) {
         cout << "Vetor_B[" << i << "] = " << Vetor_B[i] << endl;
     }
-    
+   
     system ("pause");
+
 
     return 0;
 }
