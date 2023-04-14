c3252a7c93339c830afcb96a54cd9526ef6ab5a6
Arthur Felipe Carminati <76189417+CarminatiUser@users.noreply.github.com>
2023-04-13 21:49:22 -0300

    Update ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_02.cpp

--- ProgramaçãoEmLinguagemDeMontagem/ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_02.cpp
+++ ProgramaçãoEmLinguagemDeMontagem/ArthurCarminati_HenriqueSchroeder_ViniciusGrisa_Programa_02.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-using namespace std;
-
 //Disciplina: Arquitetura e Organização de Computadores
 //Atividade: Avaliação 01 – Programação em Linguagem de Montagem
-//Programa 02
-
+//Programa 01
 
 //Grupo:
 //- Arthur Felipe Carminati
@@ -12,40 +8,62 @@ using namespace std;
 //- Vinícius Borchardt Grisa
 
 
+#include <iostream>
+
+using namespace std;
+const int TAM = 8;
+
+
 int main() {
-    int frequencia[16][32] = {0}; // inicializa todos os elementos da matriz como zero
+    int Vetor_A[TAM] = {0}; // vetor com 8 elementos inicializados em 0
+    int Vetor_B[TAM] = {0}; // vetor com 8 elementos inicializados em 0
+    int vetor_size = 0; // tamanho dos vetores
+    int temp = 0; // variável temporária para troca de valores
    
-    while (true) {
-        int aula, aluno, registro;
-       
-	  // escolhe a aula para registrar
-        cout << "Entre com o número da aula (de 0 a 15): ";
-        cin >> aula;
-       
-	  // escolhe o aluno para registrar
-        cout << "Entre com o número do aluno (de 0 a 31): ";
-        cin >> aluno;
-       
-	  // escolhe qual registro (presença ou ausência)
-        cout << "Entre com o tipo do registro (presença = 1; ausência = 0): ";
-        cin >> registro;
-       
-        frequencia[aula][aluno] = registro; // atualiza a matriz de frequência, inserindo o valor escolhido na posição da matriz
-       
-	  // confirma a atualização da tabela
-        cout << "Registro atualizado.\n";
-       
-        // mostra a nova matriz com o valor escolhido, atualizada em tempo real
-        cout << "Matriz de frequência:\n";
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 32; j++) {
-                cout << frequencia[i][j] << " ";
-            }
-            cout << endl;
-        }
+    // solicita o tamanho dos vetores
+    cout << "Entre com o tamanho dos vetores (máx. = 8): ";
+    cin >> vetor_size;
+   
+    // verifica se o tamanho é válido
+    while (vetor_size < 2 || vetor_size > TAM) {
+        cout << "Valor inválido. Entre com o tamanho dos vetores (máx. = 8): ";
+        cin >> vetor_size;
+    }
+   
+    // lê todos os elementos do Vetor_A
+    cout << "Entre com os elementos do Vetor_A:\n";
+    for (int i = 0; i < vetor_size; i++) {
+        cout << "Vetor_A[" << i << "] = ";
+        cin >> Vetor_A[i];
+    }
+   
+    // lê todos os elementos do Vetor_B
+    cout << "Entre com os elementos do Vetor_B:\n";
+    for (int i = 0; i < vetor_size; i++) {
+        cout << "Vetor_B[" << i << "] = ";
+        cin >> Vetor_B[i];
+    }
+   
+    // troca os valores dos elementos dos vetores de mesmo índice
+    for (int i = 0; i < vetor_size; i++) {
+        temp = Vetor_A[i];
+        Vetor_A[i] = Vetor_B[i];
+        Vetor_B[i] = temp;
+    }
+   
+    // imprime novamente os vetores
+    cout << "Novo Vetor_A:\n";
+    for (int i = 0; i < vetor_size; i++) {
+        cout << "Vetor_A[" << i << "] = " << Vetor_A[i] << endl;
+    }
+   
+    cout << "Novo Vetor_B:\n";
+    for (int i = 0; i < vetor_size; i++) {
+        cout << "Vetor_B[" << i << "] = " << Vetor_B[i] << endl;
     }
    
     system ("pause");
 
 
     return 0;
+}
